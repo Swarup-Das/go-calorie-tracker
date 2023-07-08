05a0029515cd4a07fdcb2ffc2c65330ff99470f4
Swarup-Das <54735020+Swarup-Das@users.noreply.github.com>
2023-07-08 12:52:14 +0530

    first commit

---
+++ server/main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"os"
+
+	"github.com/Swarup-Das/go-react-calorie-tracker/server/routes"
+	"github.com/gin-contrib/cors"
+	"github.com/gin-gonic/gin"
+)
+
+func main(){
+	port :=  os.Getenv("PORT")
+	if port == ""{
+		port="8000"
+	}
+
+	router := gin.New()
+	router.Use(gin.Logger())
+	router.Use(cors.Default())
+
+	router.POST("/entry/create", routes.AddEntry)
+
+	router.GET("/entires", routes.GetEntries)
+	router.GET("/entry/:id", routes.GetEntryById)
+	router.GET("/ingredient/:ingredient", routes.GetEntryByIngredient)
+
+	router.PUT("/entry/update/:id", routes.UpdateEntry)
+	router.PUT("/ingredient/update/:id", routes.UpdateIngredient)
+
+	router.DELETE("/entry/delete/:id", routes.DeleteEntry)
+
+	router.Run(":"+ port )
+	
+}
